c23d2782ca4ae442e4283a2f39180e267cafe769
Aribunfi <arianna@pabis.de>
2023-04-17 15:09:35 +0200

    messaggi generici di errore

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -35,3 +35,14 @@
 
     <button type="submit" class="btn btn-primary">Salva</button>
 </form>
+
+@if ($errors->any())
+  <div class="alert alert-danger">
+    <h4>Correggi i seguenti errori per proseguire: </h4>
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+@endif

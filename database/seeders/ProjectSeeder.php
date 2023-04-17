a17b664d4dd6205a2ec6b2684864dcaceb603833
Aribunfi <arianna@pabis.de>
2023-04-17 09:04:27 +0200

    comandi popolamento tabella

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -16,6 +16,15 @@ class ProjectSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for($i = 0; $i < 50; $i++) {
+            $project = new Project;
+            $project->title = $faker->word();
+            $project->year = $faker->unique()->numberBetween(1, 100);
+            $project->type = $faker->randomElement(['lunga', 'corta', 'cortissima']);
+            $project->time = $faker->numberBetween(1, 6);
+            $project->description = $faker->paragraph(8);
+            $project->img = "https://picsum.photos/300/200";
+            $project->save();
     }
+}
 }

ab16b7c0d62287e8866a56257da775df6dab613f
Aribunfi <arianna@pabis.de>
2023-04-17 09:00:57 +0200

    seeder e faker

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {

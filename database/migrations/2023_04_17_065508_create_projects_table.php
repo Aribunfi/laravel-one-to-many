871ba6dee3b9235a3b64f94dbe0a0a832978e7f7
Aribunfi <arianna@pabis.de>
2023-04-17 08:58:29 +0200

    tabella nella funzione up

--- database/migrations/2023_04_17_065508_create_projects_table.php
+++ database/migrations/2023_04_17_065508_create_projects_table.php
@@ -14,9 +14,19 @@ return new class extends Migration
     public function up()
     {
         Schema::create('projects', function (Blueprint $table) {
+            // id() genera una colonna int, primary key, AI, not null
             $table->id();
+        
+            $table->string('title', 20);
+            $table->integer('year')->unique();
+            $table->enum('type', ['graphic', 'web', 'writing']);
+            $table->integer('time')->unsigned();
+            $table->text('description')->nullable();
+            $table->text('image')->nullable();
+        
+            // timestamps() genera le colonne created_at ed updated_at
             $table->timestamps();
-        });
+          });
     }
 
     /**

5528595acc43e6312a39f354a2cdb4374eafd771
Aribunfi <arianna@pabis.de>
2023-04-17 14:05:19 +0200

    correzione doppia tabella

--- database/migrations/2023_04_17_065508_create_projects_table.php
+++ database/migrations/2023_04_17_065508_create_projects_table.php
@@ -14,15 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('projects', function (Blueprint $table) {
-            // id() genera una colonna int, primary key, AI, not null
             $table->id();
-        
-            $table->string('title', 20);
-            $table->integer('year')->unique();
-            $table->enum('type', ['graphic', 'web', 'writing']);
-            $table->integer('time')->unsigned();
-            $table->text('description')->nullable();
-            $table->text('image')->nullable();
+
+            $table->string('title');
+            $table->integer('year');
+            $table->enum('kind', ['graphic', 'web', 'writing']);
+            $table->integer('time');
+            $table->text('description');
         
             // timestamps() genera le colonne created_at ed updated_at
             $table->timestamps();
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -7,24 +7,22 @@ use Illuminate\Database\Seeder;
 use App\Models\Project;
 use Faker\Generator as Faker;
 
-class ProjectSeeder extends Seeder
-{
+class ProjectSeeder extends Seeder  {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         for($i = 0; $i < 50; $i++) {
             $project = new Project;
-            $project->title = $faker->word();
-            $project->year = $faker->unique()->numberBetween(1, 100);
-            $project->type = $faker->randomElement(['lunga', 'corta', 'cortissima']);
-            $project->time = $faker->numberBetween(1, 6);
-            $project->description = $faker->paragraph(8);
-            $project->img = "https://picsum.photos/300/200";
+            $project->title = $faker->firstNameFemale();
+            $project->year = $faker->unique()->numberBetween(2009, 2023);
+            $project->kind = $faker->randomElement(['graphic', 'web', 'writing']);
+            $project->time = $faker->unique()->numberBetween(1, 6);
+            $project->description = $faker->paragraph(12);
             $project->save();
     }
-}
-}
+}}

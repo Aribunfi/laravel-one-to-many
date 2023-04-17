f800aaeb3103dd7c3060d18444cb025971231e9d
Aribunfi <arianna@pabis.de>
2023-04-17 22:51:38 +0200

    segnaposti

--- resources/views/projects/show.index.php
+++ resources/views/projects/show.index.php
@@ -1,3 +1,14 @@
+@extends('layouts.app')
+
+@section('title', $project->title)
+
+@section('actions')
+<div>
+    <a href="{{ route(admin.projects.index}}" class="float-end btn btn-primary">Torna alla lista</a>
+</div>
+@endsection
+
+
 <strong>Title: </strong> {{ $project->title }} <br />
 <strong>Year: </strong> {{ $project->year }} <br />
 <strong>Kind: </strong> {{ $project->kind }} <br />

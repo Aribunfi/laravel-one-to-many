410ae46ce96f3e0980f67717d150c3ffb8fca4d1
Aribunfi <arianna@pabis.de>
2023-04-17 14:47:07 +0200

    form di eliminazione

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -31,5 +31,36 @@
     </tbody>
 </table>
 
+
+@foreach ($projects as $project)
+  <!-- Modal -->
+  <div class="modal fade" id="delete-modal-{{ $project->id }}" tabindex="-1" aria-labelledby="delete-modal-{{ $project->id }}-label"
+    aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="delete-modal-{{ $project->id }}-label">Conferma eliminazione</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body text-start">
+          Sei sicuro di voler eliminare il progetto {{ $project->title }} Year {{ $project->year }} con ID
+          {{ $project->id }}? <br>
+          L'operazione non è reversibile
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+
+          <form action="{{ route('projects.destroy', $project) }}" method="POST" class="">
+            @method('DELETE')
+            @csrf
+
+            <button type="submit" class="btn btn-danger">Elimina</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+@endforeach
+
 {{-- Se è stata usata la paginazione --}}
 {{ $projects->links('pagination::bootstrap-5') }}

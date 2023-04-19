d9b01a86e23dd0ab53317764f6537e4e3e9a3dcf
Aribunfi <arianna@pabis.de>
2023-04-19 18:41:04 +0200

    aggiornato database seeder

--- database/migrations/2023_04_19_155041_add_category_foreign_to_projects_table.php
+++ database/migrations/2023_04_19_155041_add_category_foreign_to_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->foreignId('category_id')->constrained();
+            $table->foreignId('category_id')->nullable()->constrained();
         });
     }
 

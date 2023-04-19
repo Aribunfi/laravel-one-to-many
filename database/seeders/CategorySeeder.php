d9ff99e529d99b536e0f2ebaeb8c3d04fc421ca5
Aribunfi <arianna@pabis.de>
2023-04-19 18:48:05 +0200

    faker e nuova categoria

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +13,15 @@ class CategorySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $labels = ["HTML 5", "CSS 3", "Javascript", "PHP", "GIT"];
+
+        foreach($labels as $label) {
+            $category = new Category();
+            $category->label = $label;
+
+            $category->save();
+        }
     }
 }

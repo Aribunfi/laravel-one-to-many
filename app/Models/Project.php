c8ec4079715839490a1e2470814de92cde108903
Aribunfi <arianna@pabis.de>
2023-04-19 17:31:12 +0200

    aggiornamento model user e project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -14,8 +14,12 @@ class Project extends Model
 
 
 class User extends Model {
-public function projects() {
-    return $this->hasMany(Post::class);
+
+protected $fillable = ["title", "year", "kind", "time", "description", "image"];
+
+
+public function user() {
+    return $this->belongsTo(User::class);
 }
 
 
